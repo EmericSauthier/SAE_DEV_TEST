2d86f82d76fb94bb679d51f4e4e416602ba84bc1
Hugoclercrenaud <Hugo.Clerc-Renaud@etu.univ-savoie.fr>
2023-01-04 09:53:13 +0100

    camera + map + gestion taille fenetre

--- Projet/Projet/Game1.cs
+++ Projet/Projet/Game1.cs
@@ -6,16 +6,23 @@ using MonoGame.Extended.Content;
 using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Serialization;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace Projet
 {
     public class Game1 : Game
     {
+        private const int LARGEUR_FENETRE = 1000, HAUTEUR_FENETRE = 800;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+
+        private OrthographicCamera _camera;
+        private Vector2 _cameraPosition;
 
         public Game1()
         {
@@ -29,6 +36,17 @@ namespace Projet
             // TODO: Add your initialization logic here
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
             Window.Title = "Test";
+
+            // Fenetre
+            _graphics.PreferredBackBufferWidth = LARGEUR_FENETRE;
+            _graphics.PreferredBackBufferHeight = HAUTEUR_FENETRE;
+            _graphics.ApplyChanges();
+
+            // Camera
+            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, LARGEUR_FENETRE, HAUTEUR_FENETRE);
+            _camera = new OrthographicCamera(viewportAdapter);
+            _cameraPosition = new Vector2(LARGEUR_FENETRE/2, HAUTEUR_FENETRE + 80);
+
             base.Initialize();
         }
 
@@ -38,7 +56,8 @@ namespace Projet
 
             // TODO: use this.Content to load your game content here
 
-            //_tiledMap = Content.Load<TiledMap>("map");
+            // Map
+            _tiledMap = Content.Load<TiledMap>("snowmap1");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
         }
 
@@ -48,19 +67,65 @@ namespace Projet
                 Exit();
 
             // TODO: Add your update logic here
+
+            // Map
             _tiledMapRenderer.Update(gameTime);
+
+            // Camera
+            MoveCamera(gameTime);
+            _camera.LookAt(_cameraPosition);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Red);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _tiledMapRenderer.Draw();
+
+            // Camera
+            _tiledMapRenderer.Draw(_camera.GetViewMatrix());
 
             base.Draw(gameTime);
+        }
+
+        private Vector2 GetMovementDirection()
+        {
+            var movementDirection = Vector2.Zero;
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Down))
+            {
+                movementDirection += Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                movementDirection -= Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Left))
+            {
+                movementDirection -= Vector2.UnitX;
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                movementDirection += Vector2.UnitX;
+            }
+
+            // Can't normalize the zero vector so test for it before normalizing
+            if (movementDirection != Vector2.Zero)
+            {
+                movementDirection.Normalize();
+            }
+
+            return movementDirection;
+        }
+
+        private void MoveCamera(GameTime gameTime)
+        {
+            var speed = 600;
+            var seconds = gameTime.GetElapsedSeconds();
+            var movementDirection = GetMovementDirection();
+            _cameraPosition += speed * movementDirection * seconds;
         }
     }
 }
