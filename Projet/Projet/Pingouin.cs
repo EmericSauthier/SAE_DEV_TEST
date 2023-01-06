bae7cb438dfaf7db02bafb1a9fc12b27c6dc0e59
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2023-01-06 11:20:12 +0100

    documentation de la classe Pingouin

--- Projet/Projet/Pingouin.cs
+++ Projet/Projet/Pingouin.cs
@@ -21,21 +21,25 @@ namespace Projet
 {
     internal class Pingouin
     {
+        // Position du pingouin et position de départ lors d'un saut
         private Vector2 position;
         private Vector2 positionSaut;
 
         private AnimatedSprite perso;
         private RectangleF hitBox;
 
+        // Paramètres de vitesse
         private double walkVelocity;
         private double slideVelocity;
         private double jumpVelocity;
         private double gravity;
 
+        // Etat du pingouin (en train de glisser, en l'air, en train de sauter)
         private bool slide;
         private bool fly;
         private bool jump;
 
+        // Constructeur
         public Pingouin(float x, float y)
         {
             this.Position = new Vector2(x, y);
@@ -46,6 +50,7 @@ namespace Projet
             this.jumpVelocity = 10;
         }
         
+        // Propriétés
         public Vector2 Position
         {
             get
@@ -121,9 +126,16 @@ namespace Projet
 
         public void Animate(bool gameOver, KeyboardState keyboardState, TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction permettant de lancer les animations et de faire bouger le pingouin
+            */
+
+            // Application de la gravité
             Gravity(mapLayer);
+
             Vector2 move = Vector2.Zero;
 
+            // Vérification de l'état de la flèche du bas
             if (keyboardState.IsKeyUp(Keys.Down))
             {
                 this.slide = false;
@@ -134,45 +146,62 @@ namespace Projet
             {
                 this.perso.Play("celebrate");
             }
+            // Si le pingouin saute (touche espace) ou est dans les airs
             else if ((keyboardState.IsKeyDown(Keys.Space) || this.fly))
             {
                 Jump(ref move, keyboardState, mapLayer);
             }
+            // Si le pingouin va à droite (flèche droite uniquement)
             else if (keyboardState.IsKeyDown(Keys.Right) && !keyboardState.IsKeyDown(Keys.Left))
             {
                 this.perso.Play("walkForward");
+                // Vérifie qu'il n'y a pas d'obstacles à droite
                 if (!CheckRight(mapLayer))
                     move = new Vector2((float)walkVelocity, 0);
             }
+            // Si le pingouin va à gauche (flèche gauche uniquement)
             else if (keyboardState.IsKeyDown(Keys.Left) && !keyboardState.IsKeyDown(Keys.Right))
             {
                 this.perso.Play("walkBehind");
+                // Vérifie qu'il n'y a pas d'obstacles à gauche
                 if (!CheckLeft(mapLayer))
                     move = new Vector2((float)-walkVelocity, 0);
             }
+            // Si le pingouin glisse (flèche du bas)
             else if (keyboardState.IsKeyDown(Keys.Down))
             {
+                // S'il n'est pas encore en train de glisser, joue l'animation où il se jète
                 if (!this.slide)
                 {
                     this.perso.Play("beforeSlide");
                     this.slide = true;
                 }
+                // S'il est déjà en trai de glisser, joue l'animation à plat ventre
                 else
                 {
                     this.perso.Play("slide");
                 }
+                // Vérifie qu'il n'y a pas d'obstacles à gauche
                 if (!CheckRight(mapLayer))
                     move = new Vector2((float)slideVelocity, 0);
             }
+            // Si aucun mouvement n'est demandé, il reste immobile
             else
             {
                 this.perso.Play("idle");
             }
 
+            // Applique le mouvement à la position du pingouin
             this.position += move;
         }
         public void Animate(String animation)
         {
+            /* 
+            (Surcharge de la fonction précédente)
+            Fonction d'animation du personnage sans déplacement
+            */
+
+            // Switch en fonction du nom de l'animation passé en paramètre
             switch (animation)
             {
                 // Joue l'animation de célébration
@@ -205,8 +234,12 @@ namespace Projet
 
         public void Jump(ref Vector2 move, KeyboardState keyboardState, TiledMapTileLayer mapLayer)
         {
-            float direction = 0;
+            /*
+            Fonction gérant le saut du pingouin
+            */
 
+            // Si le pingouin n'est pas dans les airs,
+            // il joue une animation et on attribue ces valeurs aux variables suivantes
             if (!fly)
             {
                 this.perso.Play("beforeJump");
@@ -214,25 +247,35 @@ namespace Projet
                 this.jump = true;
                 this.positionSaut = this.position;
             }
+            // S'il est déjà dans les airs, il joue l'animation suivante
             else
             {
                 this.perso.Play("jump");
             }
 
-            if (this.positionSaut.Y - this.position.Y < 80 && this.jump && !CheckTop(mapLayer))
+            // Si le pingouin est en train de sauter,
+            // que la différence de hauteur entre sa position au moment du saut et sa position actuelle est inférieur à 80
+            // et qu'il n'y a pas d'obstacles au-dessus de lui, on applique un mouvement vertical
+            // (Cela permet de fluidifier le mouvement de saut et de ne pas téléporter le pingouin)
+            if (this.jump && this.positionSaut.Y - this.position.Y < 80 && !CheckTop(mapLayer))
             {
                 move += new Vector2(0, (float)-this.jumpVelocity);
             }
+            // Sinon il n'est plus en train de sauter, il est en phase descendante
             else
             {
                 this.jump = false;
                 this.positionSaut = new Vector2();
             }
 
+            // Si l'utilisateur presse la flèche de droite et qu'il n'y a pas d'obstacles à droite,
+            // on applique un mouvement horizontal vers la droite
             if (keyboardState.IsKeyDown(Keys.Right) && !keyboardState.IsKeyDown(Keys.Left) && !CheckRight(mapLayer))
             {
                 move += new Vector2((float)walkVelocity, 0);
             }
+            // Si l'utilisateur presse la flèche de gauche et qu'il n'y a pas d'obstacles à gauche,
+            // on applique un mouvement horizontal vers la gauche
             else if (keyboardState.IsKeyDown(Keys.Left) && !keyboardState.IsKeyDown(Keys.Right) && !CheckLeft(mapLayer))
             {
                 move += new Vector2((float)-walkVelocity, 0);
@@ -240,18 +283,29 @@ namespace Projet
         }
         public void Gravity(TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction appliquant la gravité au pingouin
+            */
+
+            // S'il n'y a pas d'obstacle en-dessous du pingouin, le pingouin est en l'air
+            // on applique un mouvement vertical vers le bas
             if (!CheckBottom(mapLayer))
             {
-                this.Fly = true;
-                this.Position += new Vector2(0, (float)this.gravity);
+                this.fly = true;
+                this.position += new Vector2(0, (float)this.gravity);
             }
             else
             {
-                this.Fly = false;
+                this.fly = false;
             }
         }
         public bool CheckBottom(TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction vérifiant la présence d'obsctale en-dessous du pingouin
+            */
+
+            // Définition de deux points et de deux tiles, en bas à gauche et en bas à droite
             ushort left = (ushort)((this.Position.X - 40 * Niveau1.scale) / mapLayer.TileWidth);
             ushort right = (ushort)((this.Position.X + 40 * Niveau1.scale) / mapLayer.TileWidth);
             ushort y = (ushort)((this.Position.Y + 60 * Niveau1.scale) / mapLayer.TileHeight);
@@ -259,6 +313,7 @@ namespace Projet
             TiledMapTile? tileLeft;
             TiledMapTile? tileRight;
 
+            // Récupération des différentes tiles, si l'une a une valeur, il y a collision
             if ((mapLayer.TryGetTile(left, y, out tileLeft) != false && !tileLeft.Value.IsBlank) || (mapLayer.TryGetTile(right, y, out tileRight) != false && !tileRight.Value.IsBlank))
                 return true;
 
@@ -266,6 +321,11 @@ namespace Projet
         }
         public bool CheckTop(TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction vérifiant la présence d'obsctale au-dessus du pingouin
+            */
+
+            // Définition de deux points et de deux tiles, en haut à gauche et en haut à droite
             ushort left = (ushort)((this.Position.X - 40 * Niveau1.scale) / mapLayer.TileWidth);
             ushort right = (ushort)((this.Position.X + 40 * Niveau1.scale) / mapLayer.TileWidth);
             ushort y = (ushort)((this.Position.Y - 40 * Niveau1.scale) / mapLayer.TileHeight);
@@ -273,6 +333,7 @@ namespace Projet
             TiledMapTile? tileLeft;
             TiledMapTile? tileRight;
 
+            // Récupération des différentes tiles, si l'une a une valeur, il y a collision
             if ((mapLayer.TryGetTile(left, y, out tileLeft) != false && !tileLeft.Value.IsBlank) || (mapLayer.TryGetTile(right, y, out tileRight) != false && !tileRight.Value.IsBlank))
                 return true;
 
@@ -280,6 +341,11 @@ namespace Projet
         }
         public bool CheckLeft(TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction vérifiant la présence d'obsctale à gauche du pingouin
+            */
+
+            // Définition de deux points et de deux tiles, en haut à gauche et en bas à gauche
             ushort x = (ushort)((this.Position.X - 50 * Niveau1.scale) / mapLayer.TileWidth);
             ushort top = (ushort)((this.Position.Y + 50 * Niveau1.scale) / mapLayer.TileHeight);
             ushort bottom = (ushort)((this.Position.Y - 30 * Niveau1.scale) / mapLayer.TileHeight);
@@ -287,6 +353,7 @@ namespace Projet
             TiledMapTile? tileTop;
             TiledMapTile? tileBottom;
 
+            // Récupération des différentes tiles, si l'une a une valeur, il y a collision
             if ((mapLayer.TryGetTile(x, top, out tileTop) != false && !tileTop.Value.IsBlank) || (mapLayer.TryGetTile(x, bottom, out tileBottom) != false && !tileBottom.Value.IsBlank))
                 return true;
 
@@ -294,6 +361,11 @@ namespace Projet
         }
         public bool CheckRight(TiledMapTileLayer mapLayer)
         {
+            /*
+            Fonction vérifiant la présence d'obsctale à droite du pingouin
+            */
+
+            // Définition de deux points et de deux tiles, en haut à droite et en bas à droite
             ushort x = (ushort)((this.Position.X + 50 * Niveau1.scale) / mapLayer.TileWidth);
             ushort top = (ushort)((this.Position.Y + 50 * Niveau1.scale) / mapLayer.TileHeight);
             ushort bottom = (ushort)((this.Position.Y - 30 * Niveau1.scale) / mapLayer.TileHeight);
@@ -301,6 +373,7 @@ namespace Projet
             TiledMapTile? tileTop;
             TiledMapTile? tileBottom;
 
+            // Récupération des différentes tiles, si l'une a une valeur, il y a collision
             if ((mapLayer.TryGetTile(x, top, out tileTop) != false && !tileTop.Value.IsBlank) || (mapLayer.TryGetTile(x, bottom, out tileBottom) != false && !tileBottom.Value.IsBlank))
                 return true;
 
