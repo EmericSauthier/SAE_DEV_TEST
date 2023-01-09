754bd9c4e370d72ed0121982b3d59d2056a76aba
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2023-01-09 16:11:02 +0100

    Camera suit pingouin lorsqu'il monte trop haut

--- Projet/Projet/Camera.cs
+++ Projet/Projet/Camera.cs
@@ -56,7 +56,14 @@ namespace Projet
         public void Update(GameTime gameTime, Pingouin pingouin)
         {
             MoveCamera(gameTime);
-            CameraPosition = new Vector2(pingouin.Position.X, _hauteurFen);
+            if (pingouin.Position.Y <= _hauteurFen/ 3)
+            {
+                CameraPosition = new Vector2(pingouin.Position.X, pingouin.Position.Y);
+            } 
+            else
+            {
+                CameraPosition = new Vector2(pingouin.Position.X, _hauteurFen);
+            }
             OrthographicCamera.LookAt(CameraPosition);
         }
 
