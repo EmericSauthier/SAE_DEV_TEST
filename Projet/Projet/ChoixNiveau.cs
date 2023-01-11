6f50dff86723b806e44f0f05ddc2afcc1d48fec2
GaelleLabauve <gaelle.labauve@etu.univ-smb.fr>
2023-01-11 08:58:39 +0100

    Supression dans choix niveau de niveau3 Modification Win affichage et lien

--- Projet/Projet/ChoixNiveau.cs
+++ Projet/Projet/ChoixNiveau.cs
@@ -23,11 +23,9 @@ namespace Projet
         //CHAMPS TEXTE BOUTON
         private string _niv1;
         private string _niv2;
-        private string _niv3;
         private string _messMenu;
         private Vector2 _positionNiv1;
         private Vector2 _positionNiv2;
-        private Vector2 _positionNiv3;
         private Vector2 _positionMenu;
 
         public ChoixNiveau(Game1 game) : base(game)
@@ -38,12 +36,10 @@ namespace Projet
         {
             _niv1 = "Niveau 1";
             _niv2 = "Niveau 2";
-            _niv3 = "Niveau 3";
             _messMenu = "Retour au menu";
             _positionMenu = new Vector2(30, 50);
             _positionNiv1 = new Vector2(50, 150);
             _positionNiv2 = new Vector2(_positionNiv1.X+_niv2.Length*24+ 50, _positionNiv1.Y);
-            _positionNiv3 = new Vector2(_positionNiv2.X+_niv3.Length*24+50, _positionNiv1.Y);
 
             base.Initialize();
         }
@@ -71,10 +67,6 @@ namespace Projet
                 else if (_mouseState.X >= _positionNiv2.X && _mouseState.Y >= _positionNiv2.Y && _mouseState.X <= _positionNiv2.X + _niv2.Length * 24 && _mouseState.Y <= _positionNiv2.Y + 24)
                 {
                     _myGame.goSnow = true;
-                }//si clic niveau 3 //A CHANGER DES QUE NIVEAU 3 EST PRES
-                else if (_mouseState.X >= _positionNiv3.X && _mouseState.Y >= _positionNiv3.Y && _mouseState.X <= _positionNiv3.X + _niv3.Length * 24 && _mouseState.Y <= _positionNiv3.Y + 24)
-                {
-                    _myGame.goDead = true;
                 }
 
 
@@ -91,7 +83,6 @@ namespace Projet
             //TEXTE
             _myGame.SpriteBatch.DrawString(Game1.police, $"{_niv1}", _positionNiv1, Color.White);
             _myGame.SpriteBatch.DrawString(Game1.police, $"{_niv2}", _positionNiv2, Color.White);
-            _myGame.SpriteBatch.DrawString(Game1.police, $"{_niv3}", _positionNiv3, Color.White);
             _myGame.SpriteBatch.DrawString(Game1.police, $"{_messMenu}", _positionMenu, Color.White);
             _myGame.SpriteBatch.End();
         }
