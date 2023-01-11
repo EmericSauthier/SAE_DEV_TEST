99a858cfbfb7ad326f9fb6b4d072bf320927b117
GaelleLabauve <gaelle.labauve@etu.univ-smb.fr>
2023-01-11 10:04:11 +0100

    Amelioration regle

--- Projet/Projet/Regle.cs
+++ Projet/Projet/Regle.cs
@@ -22,12 +22,12 @@ namespace Projet
 
         public static SpriteFont policePetite;
 
-        //PINGOUIN DECO
+        //SPRITE DECO
         private Pingouin _pingouinSauter;
         private Pingouin _pingouinGlisser;
         private Pingouin _pingouinAvancer;
-        private Texture2D _textureFond;
-        private Vector2 _positionFond;
+        private Recompenses _portail;
+        private Recompenses _piece;
 
         //SOURIS POUR GERER CLIC
         private MouseState _mouseState;
@@ -37,10 +37,20 @@ namespace Projet
         private string _texteEnnemi;
         private string _texteControle;
         private string _texteIntroEnnemi;
+        private string _texteMonaie;
+        private string _texteMonaieCas1;
+        private string _texteMonaieCas2;
+        private string _texteTeleporteur;
+        private string _texteTeleporteurDesc;
         private Vector2 _positionMenu;
         private Vector2 _positiontxtEnnemi;
         private Vector2 _positiontxtControle;
         private Vector2 _positiontxtIntroEnnemi;
+        private Vector2 _positiontxtMonaie;
+        private Vector2 _positionMonaieCas1;
+        private Vector2 _positionMonaieCas2;
+        private Vector2 _positiontxtTeleport;
+        private Vector2 _positiontxtTeleportDesc;
             
             //EXPLI DEPLACEMENT
         private string _sauter;
@@ -63,7 +73,7 @@ namespace Projet
             //INITIALISATION DES TEXTES
             _menuTXT = "Menu";
                 //DEPLACEMENT
-            _texteControle = "Deplacement :";
+            _texteControle = "Action :";
             _avancer = "Avencer : fleche droite/gauche";
             _sauter = "Sauter : espace";
             _glisser = "Glisser : fleche du bas + direction";
@@ -71,27 +81,40 @@ namespace Projet
                 //ENNEMI
             _texteEnnemi = "Attention aux predateurs !";
             _texteIntroEnnemi = "Durant votre parcours, faites attention aux ennemis sur votre chemin, ils pourraient bien vous devorer ! \nEviter les ou attaquer avec la boule de neige ou en sautant sur leur dos !";
-            
+                //Recoltable
+            _texteMonaie = "Monnaie viscieuse...";
+            _texteMonaieCas1 = "Lorsque la vie n'est pas maximal : ajoute un coeur";
+            _texteMonaieCas2 = "Lorsqque vie pleine 50% de risque d'etre ralentit et 50% de chance d'etre accelere";
+            _texteTeleporteur = "Morceau de portail";
+            _texteTeleporteurDesc = " Recolter les tous afin de faire apparaitre le teleporteur sur la fin de la map !";
 
             //POSITION
             _positionMenu = new Vector2(30, 50);
 
             _positiontxtControle = new Vector2(50, 150);
-            _positionAvancer = new Vector2(_positionMenu.X, _positiontxtControle.Y + 70);
-            _positionSauter = new Vector2(_positionMenu.X+100, _positionAvancer.Y + 60);
+            _positionAvancer = new Vector2(_positionMenu.X, _positiontxtControle.Y + 50);
+            _positionSauter = new Vector2(_positionMenu.X+100, _positionAvancer.Y + 70);
             _positionGlisser = new Vector2(_positionMenu.X, _positionSauter.Y + 60);
             _positionRelever = new Vector2(_positionMenu.X, _positionGlisser.Y + 30);
-            _positionAttaquer = new Vector2(_positionMenu.X, _positionRelever.Y + 50);
+            _positionAttaquer = new Vector2(_positionMenu.X, _positionRelever.Y + 20);
 
             _positiontxtEnnemi = new Vector2(_positiontxtControle.X, _positionAttaquer.Y+60);
             _positiontxtIntroEnnemi = new Vector2(_positionMenu.X, _positiontxtEnnemi.Y+35);
 
+            _positiontxtMonaie = new Vector2(_positiontxtEnnemi.X, _positiontxtIntroEnnemi.Y + 60);
+            _positionMonaieCas1= new Vector2(_positionMenu.X, _positiontxtMonaie.Y + 35);
+            _positionMonaieCas2 = new Vector2(_positionMenu.X, _positionMonaieCas1.Y + 15);
+
+            _positiontxtTeleport = new Vector2(_positiontxtEnnemi.X, _positionMonaieCas2.Y + 60);
+            _positiontxtTeleportDesc = new Vector2(_positionMenu.X, _positiontxtTeleport.Y + 35);
+
 
             //CREATION DECO
             _pingouinSauter = new Pingouin(_positionSauter.X-80, _positionSauter.Y);
-            _pingouinAvancer = new Pingouin(_positionAvancer.X+_avancer.Length*16, _positionAvancer.Y);
-            _pingouinGlisser = new Pingouin(_positionGlisser.X+_glisser.Length*18, _positionGlisser.Y);
-            _positionFond = new Vector2(0, 0);
+            _pingouinAvancer = new Pingouin(_positionAvancer.X+_avancer.Length*12, _positionAvancer.Y-20);
+            _pingouinGlisser = new Pingouin(_positionGlisser.X+_glisser.Length*10, _positionGlisser.Y);
+            _portail = new Recompenses(new Vector2(_positiontxtTeleportDesc.X+600, _positiontxtTeleportDesc.Y), "portal", 0);
+            _piece = new Recompenses(new Vector2(_positiontxtMonaie.X+400, _positionMonaieCas1.Y-15), "coin", 0);
 
             base.Initialize();
         }
@@ -99,11 +122,14 @@ namespace Projet
         {
             //PINGOUIN DECO
             SpriteSheet spriteSheet = Content.Load<SpriteSheet>("Perso/penguin.sf", new JsonContentLoader());
+            SpriteSheet spriteSheetPortal = Content.Load<SpriteSheet>("Decors/portal.sf", new JsonContentLoader());
+            SpriteSheet spriteSheetCoin = Content.Load<SpriteSheet>("Decors/spritCoin.sf", new JsonContentLoader());
             _pingouinSauter.Perso = new AnimatedSprite(spriteSheet);
             _pingouinGlisser.Perso = new AnimatedSprite(spriteSheet);
             _pingouinAvancer.Perso = new AnimatedSprite(spriteSheet);
-
-            _textureFond = Content.Load<Texture2D>("Decors/regle2");
+            _portail.LoadContent(spriteSheetPortal);
+            _piece.LoadContent(spriteSheetCoin);
+            
 
             //POLICE
             policePetite = Content.Load<SpriteFont>("Font/FontRulesDesc");
@@ -120,6 +146,10 @@ namespace Projet
             _pingouinSauter.Perso.Update(deltaSeconds);
             _pingouinAvancer.Perso.Update(deltaSeconds);
             _pingouinGlisser.Perso.Update(deltaSeconds);
+            _portail.Sprite.Play("portal");
+            _portail.Sprite.Update(deltaSeconds);
+            _piece.Sprite.Play("coin");
+            _piece.Sprite.Update(deltaSeconds);
 
             //GESTION CLIC
             _mouseState = Mouse.GetState();
@@ -153,9 +183,21 @@ namespace Projet
             _myGame.SpriteBatch.Draw(_pingouinSauter.Perso, _pingouinSauter.Position);
             _myGame.SpriteBatch.Draw(_pingouinAvancer.Perso, _pingouinAvancer.Position);
             _myGame.SpriteBatch.Draw(_pingouinGlisser.Perso, _pingouinGlisser.Position);
+            _myGame.SpriteBatch.Draw(_portail.Sprite, _portail.Position, 0, new Vector2(2));
+            _myGame.SpriteBatch.Draw(_piece.Sprite, _piece.Position, 0, new Vector2((float)0.2));
 
             _myGame.SpriteBatch.DrawString(Game1.police, $"{_texteEnnemi}", _positiontxtEnnemi, Color.White);
             _myGame.SpriteBatch.DrawString(policePetite, $"{_texteIntroEnnemi}", _positiontxtIntroEnnemi, Color.White);
+
+
+            _myGame.SpriteBatch.DrawString(Game1.police, $"{_texteMonaie}", _positiontxtMonaie, Color.White);
+            _myGame.SpriteBatch.DrawString(policePetite, $"{_texteMonaieCas1}", _positionMonaieCas1, Color.White);
+            _myGame.SpriteBatch.DrawString(policePetite, $"{_texteMonaieCas2}", _positionMonaieCas2, Color.White);
+
+            _myGame.SpriteBatch.DrawString(Game1.police, $"{_texteTeleporteur}", _positiontxtTeleport, Color.White);
+            _myGame.SpriteBatch.DrawString(policePetite, $"{_texteTeleporteurDesc}", _positiontxtTeleportDesc, Color.White);
+
+
             _myGame.SpriteBatch.End();
         }
     }
